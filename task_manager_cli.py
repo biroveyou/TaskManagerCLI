26d762984091cb0ceb1b15df610a36a6bae25414
biroveyou <biroveyou@gmail.com>
2026-05-02 09:54:55 -0300

    Implement 'list' feature into the CLI

--- task_manager_cli.py
+++ task_manager_cli.py
@@ -1,5 +1,12 @@
 import argparse
+import pprint
 
+# ID : {Title: "", tag: ""}
+tasks = {1:{"Title": "Buy groceries", "tag": "todo"},
+         2:{"Title": "Buy milk", "tag": "in-progress"},
+         3:{"Title": "Shave", "tag": "done"}}
+
+# Functions
 def task_add(args):
     print("Task added successfully (ID: 1)")
 
@@ -9,6 +16,23 @@ def task_delete(args):
 def task_update(args):
     print("Task updated (ID: 1)")
 
+def task_list(args):
+    if args.tag == "todo":
+        for k, v in tasks.items():
+            if tasks[k]["tag"] == args.tag:
+                print(tasks[k])
+    elif args.tag == "done":
+        for k, v in tasks.items():
+            if tasks[k]["tag"] == args.tag:
+                print(tasks[k])
+    elif args.tag == "in-progress":
+        for k, v in tasks.items():
+            if tasks[k]["tag"] == args.tag:
+                print(tasks[k])
+    else:
+        pprint.pprint(tasks)
+
+# Main function
 def main(): 
     parser = argparse.ArgumentParser(
         prog        = "task-manager",
@@ -19,11 +43,17 @@ def main():
     parser_add = subparsers.add_parser("add", help="Add a task")
     parser_add.set_defaults(func=task_add)
 
-    parser_delete = subparsers.add_parser("delete", help="deletes a task")
+    parser_delete = subparsers.add_parser("delete", help="Deletes a task")
     parser_delete.set_defaults(func=task_delete)
 
     parser_update = subparsers.add_parser("update", help="Updates the status of a task")
     parser_update.set_defaults(func=task_update)
+
+    parser_list = subparsers.add_parser("list", help ="List tasks")
+    parser_list.add_argument("tag", nargs="?",
+                             choices=["todo", "done", "in-progress", "all"], 
+                             default="all")
+    parser_list.set_defaults(func=task_list)
 
     args = parser.parse_args()
 
